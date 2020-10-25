f0b33eac255fa1209705eea406ebba101670f254
Raul Gomez <83raulgomez@gmail.com>
2020-10-25 12:01:26 +0100

    hooks

---
+++ useCounter.js
@@ -0,0 +1,17 @@
+import {useState} from "react"
+
+export const useCounter = (initialState=15) => {
+
+   const [counter, setCounter] = useState(initialState);
+
+   const increment=()=>{
+       setCounter(counter+1);
+   }
+   const reset=()=>{
+       setCounter(initialState)
+   }
+   const decrement=()=>{
+       setCounter(counter-1)
+   }
+   return{counter,increment,reset,decrement}
+}
---
+++ useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect,useRef } from 'react';
+
+
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true)
+    const [state,setState]=useState({data:null,loading:true,error:null});
+
+    useEffect(() => {
+        return () => {
+           isMounted.current=false;
+        }
+    }, [])
+
+    useEffect(() => {
+        setState({data:null,loading:true,error:null}) //efecto loading en cada recarga, paso de frase
+
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            if(isMounted.current){
+                setState({
+                    loading:false, error:null, data
+                });
+            }
+        });
+    }, [url])
+
+    return state
+}
